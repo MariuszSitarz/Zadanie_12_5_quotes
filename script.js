069ad0b84e291793bef27a3dc0aee21b9fb8f114
Mariusz Sitarz <mariusz_sitarz@wp.pl>
2017-10-22 23:56:19 +0200

    added tweet sound

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ document.addEventListener("DOMContentLoaded", function () {
         getQuote();
     });
 
+    $('.tweet').click(function () {
+        $('#audio')[0].play();
+    });
+
     var prefix = "https://cors-anywhere.herokuapp.com/";
     var tweetLink = "https://twitter.com/intent/tweet?text=";
     var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
